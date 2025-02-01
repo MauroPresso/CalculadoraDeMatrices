cfe7d8260a8051841657f1637c4f87ab4d587e4d
MauroPresso <mauro.presso@gmail.com>
2025-02-01 20:35:16 -0300

    This is the full-filled version of this file.

---
+++ calculation_options_lib.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <stdint.h>
+
+void handle_matrix_addition(void);
+
+void handle_matrix_substraction(void);
+
+void handle_matrices_multiplication(void);
+
+void handle_matrix_and_scalar_multiplication(void);
+
+void handle_matrix_transpose(void);
+
+void handle_matrix_determinant(void);
+
+void handle_matrix_inverse(void);
