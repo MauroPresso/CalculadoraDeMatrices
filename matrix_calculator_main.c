29efc05854d8e8850586ab9604cfc588fb87671d
MauroPresso <mauro.presso@gmail.com>
2025-02-01 20:29:42 -0300

    Se agregaron todas las funciones. Falta compilar.

--- matrix_calculator_main.c
+++ matrix_calculator_main.c
@@ -16,25 +16,31 @@ int main()
                 break; // El break es para que no siga ejecutando secuencialmente los otros casos.
             case 1:
                 printf("\n[Operación seleccionada] Suma de matrices.\n");
-                // Llamar a la función correspondiente
+                handle_matrix_addition();
                 break;
             case 2:
                 printf("\n[Operación seleccionada] Resta de matrices.\n");
+                handle_matrix_subtraction();
                 break;
             case 3:
                 printf("\n[Operación seleccionada] Multiplicación de matrices.\n");
+                handle_matrices_multiplication();
                 break;
             case 4:
                 printf("\n[Operación seleccionada] Multiplicación por escalar.\n");
+                handle_matrix_and_scalar_multiplication();
                 break;
             case 5:
                 printf("\n[Operación seleccionada] Transposición de matriz.\n");
+                handle_matrix_transpose();
                 break;
             case 6:
                 printf("\n[Operación seleccionada] Cálculo del determinante.\n");
+                handle_matrix_determinant();
                 break;
             case 7:
                 printf("\n[Operación seleccionada] Cálculo de matriz inversa.\n");
+                handle_matrix_inverse();
                 break;
             // default: sentencia; /*opcional porque estoy seguro que todos los valores de el selector estan incluidos en las etiquetas case*/
         }
