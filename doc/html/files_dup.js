6a4ade8e79bbab3659ca5c457e146e9732a0eac9
Mauro <mauro.presso@gmail.com>
2025-07-19 13:55:03 -0300

    Documentación del mini-proyecto de la calculadora de matrices. Todo funciona perfecto.

---
+++ doc/html/files_dup.js
@@ -0,0 +1,5 @@
+var files_dup =
+[
+    [ "calculation_options_lib.h", "calculation__options__lib_8h_source.html", null ],
+    [ "matrix_calculator_lib.h", "matrix__calculator__lib_8h_source.html", null ]
+];
---
+++ doc/html/navtreeindex0.js
@@ -0,0 +1,8 @@
+var NAVTREEINDEX0 =
+{
+"calculation__options__lib_8h_source.html":[0,0,0],
+"files.html":[0,0],
+"index.html":[],
+"matrix__calculator__lib_8h_source.html":[0,0,1],
+"pages.html":[]
+};
